6417109c112dc51aa0fb44c4d37d9821907f1f2b
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-30 22:30:01 -0700

    Rename DkMLReleaseParticipant to DkMLBumpVersionParticipant

--- bump-version.cmake
+++ bump-version.cmake
@@ -1,11 +1,11 @@
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-compiler -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLReleaseParticipant.cmake -P bump-version.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-compiler -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
 
 if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
-    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLReleaseParticipant.cmake")
+    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
 endif()
 include(${DKML_RELEASE_PARTICIPANT_MODULE})
 
-DkMLReleaseParticipant_OpamReplace(dkml-compiler-env.opam)
-DkMLReleaseParticipant_DkmlBaseCompilerReplace(dkml-base-compiler.opam)
-DkMLReleaseParticipant_GitAddAndCommit()
+DkMLBumpVersionParticipant_OpamReplace(dkml-compiler-env.opam)
+DkMLBumpVersionParticipant_DkmlBaseCompilerReplace(dkml-base-compiler.opam)
+DkMLBumpVersionParticipant_GitAddAndCommit()
